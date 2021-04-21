c974fa45af4348e508cc83220cb5354f58790882
Jovanny Rch <jovannyrch@gmail.com>
2021-04-20 22:17:58 -0500

    Add project files.

---
+++ ProyectoPA/Source.cpp
@@ -0,0 +1,130 @@
+#include <Windows.h>
+#include "resource.h"
+
+
+BOOL CALLBACK fVentanaPrincipal(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK fRegisterWindow(HWND, UINT, WPARAM, LPARAM);
+
+void handleLoginClickButton(HWND);
+void cerrarVentana(HWND);
+void setEnable(HWND, UINT, bool);
+void handleRegisterClickButton(HWND);
+void createRegisterWindow();
+void createLoginWindow();
+
+HINSTANCE hGlobalInstance;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrev, PSTR cmdLine, int cShow) {
+	
+	hGlobalInstance = hInstance;
+
+	createLoginWindow();
+	
+	MSG msg;
+	ZeroMemory(&msg, sizeof(MSG));
+
+	while (GetMessage(&msg, 0, 0, 0)) {
+		TranslateMessage(&msg);
+
+		DispatchMessage(&msg);
+	}
+
+
+	return 1;
+}
+
+BOOL CALLBACK fVentanaPrincipal(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	switch (msg)
+	{
+		
+		case WM_COMMAND:
+		{
+			
+
+			if (HIWORD(wparam) == BN_CLICKED) {
+				switch (LOWORD(wparam))
+				{
+					case BTN_LOGIN:
+						handleLoginClickButton(hwnd);
+						break;
+					case BTN_LOGIN_SALIR:
+						cerrarVentana(hwnd);
+						break;
+					case BTN_REGISTRARSE:
+						handleRegisterClickButton(hwnd);
+						break;
+				}	
+			}
+
+		}
+		break;
+		case WM_CLOSE:
+			DestroyWindow(hwnd);
+		break;
+		case WM_DESTROY:
+			PostQuitMessage(11);
+			break;
+	}
+
+	return false;
+}
+
+
+void handleLoginClickButton(HWND hwnd) {
+
+	char nombre[50];
+	HWND hTxtNombre = GetDlgItem(hwnd, TXT_NOMBRE);
+	int iTxtLength = GetWindowTextLength(hTxtNombre);
+
+	if (iTxtLength == 0) {
+		MessageBox(hwnd, "Ingrese nombre", "Valor vacío", MB_ICONEXCLAMATION);
+
+	}
+	GetWindowText(hTxtNombre, nombre, ++iTxtLength);
+	MessageBox(hwnd, nombre, "titulo", MB_HELP);
+
+}
+
+
+void setEnable(HWND hwnd, UINT id, bool isEnable) {
+	EnableWindow(GetDlgItem(hwnd, id), isEnable);
+}
+
+
+void handleRegisterClickButton(HWND hwnd) {
+	ShowWindow(hwnd, SW_HIDE);
+	createRegisterWindow();
+}
+
+
+void createRegisterWindow() {
+	HWND registerwindow = CreateDialog(hGlobalInstance, MAKEINTRESOURCE(1), NULL, fRegisterWindow);
+	ShowWindow(registerwindow, SW_SHOW);
+}
+
+void createLoginWindow() {
+	HWND hVentanaPrincipal = CreateDialog(hGlobalInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, fVentanaPrincipal);
+	ShowWindow(hVentanaPrincipal, SW_SHOW);
+}
+
+BOOL CALLBACK fRegisterWindow(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	switch (msg)
+	{
+
+	case WM_COMMAND:
+	{
+
+
+
+	}
+	break;
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(11);
+		break;
+	}
+
+	return false;
+}
---
+++ ProyectoPA/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ProyectoPA.rc
+//
+#define IDD_DIALOG1                     101
+#define IDD_DIALOG2                     103
+#define BTN_LOGIN                       1001
+#define BTN_LOGIN_SALIR                 1002
+#define TXT_NOMBRE                      1003
+#define TXT_PASSWORD                    1005
+#define BTN_REGISTRARSE                 1007
+#define BTN_REGISTER_SUBMIT             1010
+#define BTN_REGISTER_GO_BACK            1011
+
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
