3e0042496356eab28bc76b5427c95dd71119df96
Jovanny Rch <jovannyrch@gmail.com>
2021-04-22 13:31:12 -0500

    add:screens

--- ProyectoPA/resource.h
+++ ProyectoPA/resource.h
@@ -12,6 +12,10 @@
 #define ENVIOS_EDITAR                   114
 #define ENVIOS_ALTA                     116
 #define PRODUCTOS_ALTAS                 118
+#define PRODUCTOS_BAJAS                 120
+#define PRODUCTOS_CAMBIOS               122
+#define IDD_DIALOG1                     124
+#define PRODUCTOS_LISTA                 124
 #define BTN_LOGIN                       1001
 #define BTN_LOGIN_SALIR                 1002
 #define TXT_NOMBRE                      1003
@@ -29,6 +33,7 @@
 #define IDC_BUTTON1                     1024
 #define IDC_BUTTON2                     1026
 #define IDC_EDIT2                       1027
+#define IDC_BUTTON4                     1028
 #define IDC_EDIT3                       1029
 #define IDC_EDIT4                       1030
 #define IDC_EDIT5                       1031
@@ -55,7 +60,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        120
+#define _APS_NEXT_RESOURCE_VALUE        126
 #define _APS_NEXT_COMMAND_VALUE         40009
 #define _APS_NEXT_CONTROL_VALUE         1041
 #define _APS_NEXT_SYMED_VALUE           101
