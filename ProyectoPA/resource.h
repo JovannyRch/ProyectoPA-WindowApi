3d7a3bb7fcde44da70fc2d3404856bdabad9ce08
Jovanny Rch <jovannyrch@gmail.com>
2021-04-22 01:33:50 -0500

    Add : Cancelar Envio Screen

--- ProyectoPA/resource.h
+++ ProyectoPA/resource.h
@@ -7,8 +7,8 @@
 #define SCREEN_INFORMACION_VENDEDOR     105
 #define SCREEN_ENVIOS                   107
 #define IDR_MENU1                       109
-#define IDD_DIALOG1                     110
 #define ENVIOS_MIS_ENVIOS               110
+#define ENVIOS_CANCELAR                 112
 #define BTN_LOGIN                       1001
 #define BTN_LOGIN_SALIR                 1002
 #define TXT_NOMBRE                      1003
@@ -23,6 +23,7 @@
 #define BTN_SUBIR_FOTO                  1021
 #define BTN_REGRESAR_INFO_VENDEROR      1022
 #define IDC_LIST1                       1023
+#define IDC_BUTTON1                     1024
 #define ID_PRODUCTOS_ALTAS              40001
 #define ID_PRODUCTOS_BAJAS              40002
 #define ID_PRODUCTOS_CAMBIOS            40003
@@ -36,9 +37,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        112
+#define _APS_NEXT_RESOURCE_VALUE        114
 #define _APS_NEXT_COMMAND_VALUE         40009
-#define _APS_NEXT_CONTROL_VALUE         1024
+#define _APS_NEXT_CONTROL_VALUE         1025
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
